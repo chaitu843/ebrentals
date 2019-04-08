f49653d8d99f3894fe69a48325d3683ea66f2170
credd3 <creddy4@sapient.com>
2019-04-08 19:10:09 +0530

    Displaying products and adding them to cart functionality is done!

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,56 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import Cards from '../Cards/Cards';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       products : []
+    }
+    this.url = "http://localhost:3001";
+  }
+  
+  componentDidMount() {
+    fetch(`${this.url}/products`)
+    .then(resp => resp.json())
+    .then(data => this.setState({
+      products: data,
+    }))
+  }
+  addToCart = (id) => {
+    fetch(`${this.url}/products/${id}`)
+      .then(resp => resp.json())
+      .then(data => {
+        fetch(`${this.url}/products/${id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            addedToCart: !data.addedToCart,
+          }),
+
+        })
+        .then(resp =>  this.setState({
+          products: this.state.products.map(product => {
+            if(product.id===id) {
+               product.addedToCart = !product.addedToCart;
+            }
+            return product;
+          })
+        }))
+      }) // Getting the data from server.. updating it and then updating the state
+  }
+
   render() {
-    return ;
+    return (
+      <div className="container">
+        <Cards products={this.state.products} addToCart={this.addToCart}/>
+      </div>
+    )
   }
 }
 
